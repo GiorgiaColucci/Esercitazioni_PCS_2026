f392718f654817b3065b99f90e9b8c875ce25005
Giorgia Colucci <gigra@MacBook-Pro-di-Giorgia.local>
2026-03-16 01:47:36 +0100

    file media

--- Esercitazione 1/media.cpp
+++ Esercitazione 1/media.cpp
@@ -3,23 +3,29 @@
 
 using namespace std;
 
-int main(void) {
-cout << "Inserisci il nome del file: \n";
-string filename;
-cin >> filename;
-ifstream ifs(filename);
-if ( ifs.is_open() ) { 
+int main(int argc, char*argv[]) {
+	if (argc < 2) {
+		cerr << "Errore: devi specificare il nome del file \n";
+		return 1;
+	}
+	
+	string filename = argv[1];
+	ifstream ifs(filename);
+	
+	if (!ifs.is_open()) {
+		cerr << "Errore: impossibile aprire il file" << filename << " \n";
+		return 1;
+	}
+		 
 	for (int i = 0; i < 3; i++) {
 		string city;
-		double temp1;
-		double temp2;
-		double temp3;
-		double temp4;
+		double temp1, temp2, temp3, temp4;
 		double avg;
-	ifs >> city >> temp1 >> temp2 >> temp3 >> temp4;
-	avg = (temp1 + temp2 + temp3 + temp4)/4;
-	cout << "La temperatura media a" << city << "è di" << avg << "gradi" << "\n";
+		
+		ifs >> city >> temp1 >> temp2 >> temp3 >> temp4;
+		avg = (temp1 + temp2 + temp3 + temp4)/4;
+		
+		cout << "La temperatura media a " << city << " è di " << avg << " gradi " << "\n";
 	}
-} 
-return 0;
+	return 0;
 }
